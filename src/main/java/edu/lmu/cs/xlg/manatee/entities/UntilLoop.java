57c9ce535a61027dff5b79902e79248dad8c7c32
Don Rowe <rowedonalde@gmail.com>
2012-04-03 02:50:24 -0700

    Initialized the entity for UntilLoop

---
+++ src/main/java/edu/lmu/cs/xlg/manatee/entities/UntilLoop.java
@@ -0,0 +1,11 @@
+package edu.lmu.cs.xlg.manatee.entities;
+
+import edu.lmu.cs.xlg.util.Log;
+
+/**
+ * A Manatee UntilLoop object
+ */
+
+public class UntilLoop extends Statement { //Might want to just make this extend WhileLoop
+
+}
