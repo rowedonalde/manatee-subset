e3ab61de682dc1ffd5af1e940ebda00290a22f7f
Don Rowe <rowedonalde@gmail.com>
2012-04-03 02:55:18 -0700

    Initialized entity class for TryStatement

---
+++ src/main/java/edu/lmu/cs/xlg/manatee/entities/TryStatement.java
@@ -0,0 +1,11 @@
+package edu.lmu.cs.xlg.manatee.entities;
+
+import edu.lmu.cs.xlg.util.Log;
+
+/**
+ * A Manatee try statement object for handling errors
+ */
+
+public class TryStatement extends Statement {
+
+}
