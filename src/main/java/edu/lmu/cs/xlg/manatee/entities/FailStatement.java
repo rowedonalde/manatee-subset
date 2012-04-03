f6c09a57e37180451c3075553c8246e3b8d3fe6a
Don Rowe <rowedonalde@gmail.com>
2012-04-03 02:47:25 -0700

    Initialized the entity class for FailStatement

---
+++ src/main/java/edu/lmu/cs/xlg/manatee/entities/FailStatement.java
@@ -0,0 +1,11 @@
+package edu.lmu.cs.xlg.manatee.entities;
+
+import edu.lmu.cs.xlg.util.Log;
+
+/**
+ * A Manatee error throwing statement object
+ */
+
+public class FailStatement extends Statement {
+
+}
