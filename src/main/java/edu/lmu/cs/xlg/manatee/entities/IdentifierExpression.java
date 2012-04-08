f47808fd0f90be6f04537d6716c375f80f271f5d
Don Rowe <rowedonalde@gmail.com>
2012-04-08 01:21:18 -0700

    Added new constructor, fields, and getters for object literal support in the IdentifierExpression entity

--- src/main/java/edu/lmu/cs/xlg/manatee/entities/IdentifierExpression.java
+++ src/main/java/edu/lmu/cs/xlg/manatee/entities/IdentifierExpression.java
@@ -1,6 +1,8 @@
 package edu.lmu.cs.xlg.manatee.entities;
 
 import edu.lmu.cs.xlg.util.Log;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * An expression consisting of a single identifier, a referencing occurrence to a variable or a
@@ -10,13 +12,36 @@ public class IdentifierExpression extends Expression {
 
     private String name;
     private Entity referent;
-
+    private List<String> propertyNames;
+    private List<Expression> propertyValues;
+    
+    /**
+     * Constructor for simple ID:
+     */
     public IdentifierExpression(String name) {
         this.name = name;
+    }
+    
+    /**
+     * Constructor for object literal:
+     */
+    public IdentifierExpression(String name, List<String> propertyNames,
+                                List<Expression> propertyValues) {
+        this.name = name;
+        this.propertyNames = propertyNames;
+        this.propertyValues = propertyValues;
     }
 
     public String getName() {
         return name;
+    }
+    
+    public List<String> getPropertyNames() {
+        return this.propertyNames;
+    }
+    
+    public List<Expression> getPropertyValues() {
+        return this.propertyValues;
     }
 
     public Entity getReferent() {
