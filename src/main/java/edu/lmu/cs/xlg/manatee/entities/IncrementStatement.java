da511bf97160286ba8b4ac9e16d4e1b2dbd9bd57
Don Rowe <rowedonalde@gmail.com>
2012-04-23 23:04:52 -0700

    Initialized IncrementStatement.java Entity

---
+++ src/main/java/edu/lmu/cs/xlg/manatee/entities/IncrementStatement.java
@@ -0,0 +1,11 @@
+package edu.lmu.cs.xlg.manatee.entities;
+
+import edu.lmu.cs.xlg.util.Log;
+
+/**
+ * An increment statement. The converse "decrement" statement is also
+ * covered in this class.
+ */
+public class IncrementStatement extends AssignmentStatement {
+    
+}
