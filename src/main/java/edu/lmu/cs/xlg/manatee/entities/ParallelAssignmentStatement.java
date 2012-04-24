a2d53d1f44512b080aeb79ea113e3e2b26abad38
Don Rowe <rowedonalde@gmail.com>
2012-04-24 01:03:39 -0700

    Added analyzer to ParallelAssignmentStatement.java

--- src/main/java/edu/lmu/cs/xlg/manatee/entities/ParallelAssignmentStatement.java
+++ src/main/java/edu/lmu/cs/xlg/manatee/entities/ParallelAssignmentStatement.java
@@ -12,7 +12,7 @@ public class ParallelAssignmentStatement extends Statement {
     private List<Expression> targets;
     private Expression source;
     
-    public AssignmentStatement(List<Expression> targets, Expression source) {
+    public ParallelAssignmentStatement(List<Expression> targets, Expression source) {
         this.targets = targets;
         this.source = source;
     }
@@ -23,5 +23,16 @@ public class ParallelAssignmentStatement extends Statement {
     
     public Expression getSource() {
         return source;
+    }
+    
+    @Override
+    public void analyze(Log log, SymbolTable table, Subroutine owner, boolean inLoop) {
+        //For each Expression in targets, just build a new vanilla
+        //AssignmentStatement and let that class do the work:
+        for (int i = 0; i < targets.size(); i++) {
+            Expression target = targets.get(i);
+            AssignmentStatement current = new AssignmentStatement(target, source);
+            current.analyze(log, table, owner, inLoop);
+        }
     }
 }
