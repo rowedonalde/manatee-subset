fa018a7ff50607dfa097afd0c35c26a93072c965
don <drowe2@lion.lmu.edu>
2012-04-27 16:56:56 -0700

    Added support for true parallel assignment

--- src/main/java/edu/lmu/cs/xlg/manatee/entities/ParallelAssignmentStatement.java
+++ src/main/java/edu/lmu/cs/xlg/manatee/entities/ParallelAssignmentStatement.java
@@ -10,27 +10,34 @@ import java.util.List;
 public class ParallelAssignmentStatement extends Statement {
     
     private List<Expression> targets;
-    private Expression source;
+    private List<Expression> sources;
     
-    public ParallelAssignmentStatement(List<Expression> targets, Expression source) {
+    public ParallelAssignmentStatement(List<Expression> targets, List<Expression> sources) {
         this.targets = targets;
-        this.source = source;
+        this.sources = sources;
     }
     
     public List<Expression> getTargets() {
         return targets;
     }
     
-    public Expression getSource() {
-        return source;
+    public List<Expression> getSources() {
+        return sources;
     }
     
     @Override
     public void analyze(Log log, SymbolTable table, Subroutine owner, boolean inLoop) {
+        //First, make sure that there are an equal number of
+        //Expressions in each of targets and sources:
+        if (targets.size() != sources.size()) {
+            log.error("unbalanced.parallel.assignment");
+        }
+        
         //For each Expression in targets, just build a new vanilla
         //AssignmentStatement and let that class do the work:
         for (int i = 0; i < targets.size(); i++) {
             Expression target = targets.get(i);
+            Expression source = sources.get(i);
             AssignmentStatement current = new AssignmentStatement(target, source);
             current.analyze(log, table, owner, inLoop);
         }
