10164891be0fe9d5bf041ec5c8f0d15e61f3f82f
don <drowe2@lion.lmu.edu>
2012-04-29 17:18:47 -0700

    Added constructor and getters to PropertyExpression.java

--- src/main/java/edu/lmu/cs/xlg/manatee/entities/PropertyExpression.java
+++ src/main/java/edu/lmu/cs/xlg/manatee/entities/PropertyExpression.java
@@ -3,5 +3,19 @@ package edu.lmu.cs.xlg.manatee.entities;
 import edu.lmu.cs.xlg.util.Log;
 
 public class PropertyExpression extends Expression {
-
+    private Expression object;
+    private String propertyName;
+    
+    public PropertyExpression(Expression object, String propertyName) {
+        this.object = object;
+        this.propertyName = propertyName;
+    }
+    
+    public Expression getObject() {
+        return this.object;
+    }
+    
+    public String getPropertyName() {
+        return this.propertyName;
+    }
 }
